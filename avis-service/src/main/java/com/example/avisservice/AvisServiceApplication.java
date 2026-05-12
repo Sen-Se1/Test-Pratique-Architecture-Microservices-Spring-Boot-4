5f8e3ecc566962b7bc3feb325757895cbc0e8ec8
SenSe1 <102629374+Sen-Se1@users.noreply.github.com>
2026-05-12 08:55:30 +0100

    add Categorie and Produit entity classes

---
+++ avis-service/src/main/java/com/example/avisservice/AvisServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.avisservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvisServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvisServiceApplication.class, args);
+    }
+
+}
--- produits-service/src/main/java/com/example/produitsservice/entity/Produit.java
+++ produits-service/src/main/java/com/example/produitsservice/entity/Produit.java
@@ -1,4 +1,20 @@
 package com.example.produitsservice.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Produit {
+    @Id
+    private Long id;
+
+    private String nom;
+    private double prix;
+    
 }
